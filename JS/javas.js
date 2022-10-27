e9f09759debde8370a35a4d3429821f5c32356f1
Joanna Skrzypkowska <yoa.s.ja@wp.pl>
2022-10-27 22:30:08 +0200

    wiadomosc

---
+++ JS/javas.js
@@ -0,0 +1,8 @@
+let przycisk = document.querySelector(".przycisk");
+let naglowek = document.querySelector(".naglowek");
+
+przycisk.addEventListener("click", () => {
+    
+    naglowek.remove("naglowek");
+});
+
